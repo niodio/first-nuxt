242dafa1b26e9e79401cb2197b516d24e7d0cbef
Adelson Inácio <acinacio@gmail.com>
2022-05-01 13:50:34 -0300

    commit initial

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-gray-100">
+    <div
+      class="container mx-auto space-x-4 flex justify-center items-center border"
+    >
+      <NuxtLink to="/a-empresa"> A Empresa </NuxtLink>
+      <NuxtLink to="/servicos"> Serviços </NuxtLink>
+      <NuxtLink to="/produtos"> Produtos </NuxtLink>
+      <NuxtLink to="/produtos/prod"> PROD </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
---
+++ pages/a-empresa.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header />
+
+    <div class="container mx-auto">
+      <h1 class="text-lg font-bold">A Empresa</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+        molestias voluptatibus magni aperiam repellendus nemo architecto,
+        necessitatibus tenetur mollitia quas, laborum voluptas ipsa qui?
+        Expedita quisquam animi laboriosam quaerat saepe? Sit a quo rem
+        consectetur, perferendis perspiciatis doloremque illo ex, officiis
+        voluptatibus rerum obcaecati maiores possimus, iusto suscipit et
+        architecto eos illum. Iste reiciendis odio illum quos aut veritatis
+        iure. Fugit vel vitae illum similique eius voluptatum deleniti
+        voluptates ipsam corporis reprehenderit nisi iusto numquam voluptatibus
+        quos veritatis, iure quae ex nesciunt excepturi recusandae expedita
+        laborum explicabo nostrum. Perferendis, tempore.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+export default {
+  name: "",
+  components: { Header },
+};
+</script>
+
+<style></style>
